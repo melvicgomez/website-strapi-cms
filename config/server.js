78d5cfe44ec6462ba50b9083e5470b9211208017
Melvic Gomez <melvicgomez.ph@gmail.com>
2020-11-30 17:49:38 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c195967f67637be0c1717c7e5e55fe07'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a47629e4-d98c-4b89-9d89-0ad327f77cec'
+};
