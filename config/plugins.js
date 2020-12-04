0c6de584932d39bebe3d1956f74104dd0f0abbeb
Melvic Gomez <melvicgomez.ph@gmail.com>
2020-12-04 15:25:14 +0800

    Removed AWS_ prefixes

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "aws-s3",
     providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION_NAME"),
+      accessKeyId: env("ACCESS_KEY_ID"),
+      secretAccessKey: env("ACCESS_SECRET"),
+      region: env("REGION_NAME"),
       params: {
-        Bucket: env("AWS_S3_BUCKET_NAME"),
+        Bucket: env("S3_BUCKET_NAME"),
       },
     },
   },
